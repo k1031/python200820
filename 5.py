ee2691de65f266d46985c0790ed15b52d931c303
k1031 <69790551+k1031@users.noreply.github.com>
2020-08-20 15:36:57 +0800

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 15:28:49 2020
+
+@author: user
+"""
+
+#更新儲存格
+update_acell()和update_cell()
+#插入新列
+insert_row()
+#讀取儲存格
+acell()和cell()
+#讀取整列或整欄
+row_values()和col_values()
+#傳回整個工作表
+get_all_values()
